68e266966b4f2c273ad42731919091493cb9427b
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-08 11:01:46 +0530

    gap between menu icon and header name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link"
+import Menu from "./Menu"
+
 const Navbar = () => {
     return (
       // h-20 means 80 pixel, making responsive screen sizes using tailwind css
-      <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'> Navbar </div>
+      <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64"> 
+        <div className="h-full flex items-center justify-between">  {/*gap between menu icon and header name */}
+        
+          {/* Mobile - menu bar for only mobile users*/}
+          <Link href="/">LAMA</Link>
+          <Menu/>
+
+        </div>
+
+      </div>
     )
   }
   
