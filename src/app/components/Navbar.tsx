90d462ffb503311e5b9f650a77214394055fd4ec
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-08 10:26:35 +0530

    Making responsive screen sizes using tailwind css

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 const Navbar = () => {
     return (
-      <div> Navbar </div>
+      // h-20 means 80 pixel, making responsive screen sizes using tailwind css
+      <div className='h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'> Navbar </div>
     )
   }
   
