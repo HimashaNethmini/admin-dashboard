f3c545b041bec787c58a3aeb3bf4704abf25380a
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-12 16:47:21 +0530

    right side of the homepage

--- src/app/components/Navicons.tsx
+++ src/app/components/Navicons.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
 
 const Navicons = () => {
-  return (
-    <div>Navicons</div>
-  )
-}
+  //use state
+  const [isProfileOpen, setisProfileOpen] = useState(false);
+  const [isCartOpen, setisCartOpen] = useState(false);
 
-export default Navicons
+  return (
+    // right side of the homepage
+    <div className="flex items-center gap-4 xl:gap-6">
+      <Image
+        src="/profile.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      {isProfileOpen && <div className=""></div>}
+      <Image
+        src="/notification.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <Image
+        src="/cart.png"
+        alt=""
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default Navicons;
