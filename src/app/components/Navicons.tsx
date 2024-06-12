e441d56e50448799c70f1a2f72c7f94cd1a16e2a
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-12 17:15:32 +0530

    create a login handle function

--- src/app/components/Navicons.tsx
+++ src/app/components/Navicons.tsx
@@ -2,11 +2,29 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Navicons = () => {
+
+  //login route
+  const router = useRouter();
+
   //use state
-  const [isProfileOpen, setisProfileOpen] = useState(false);
-  const [isCartOpen, setisCartOpen] = useState(false);
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  //tempprary 
+  const isLoggedIn = false
+
+  //handle login function
+  const handleProfile = () =>{
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen ((prev) => !prev);
+    };
+  
 
   return (
     // right side of the homepage
@@ -17,8 +35,14 @@ const Navicons = () => {
         width={22}
         height={22}
         className="cursor-pointer"
+        onClick={handleProfile}
       />
-      {isProfileOpen && <div className=""></div>}
+      {isProfileOpen && <div className="">
+        <Link href="/">Profile</Link>
+        <div className="mt-2 cursor-pointer">Logout</div>
+      </div>}
+
+
       <Image
         src="/notification.png"
         alt=""
