25595cd313347c0502f4e4b85c291d7627fc19f9
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-13 13:05:51 +0530

    updated cart modal

--- src/app/components/CartModal.tsx
+++ src/app/components/CartModal.tsx
@@ -8,6 +8,9 @@ const CartModal = () => {
       {!cartItems ? (
         <div className="">Cart is Empty </div>
       ) : (
+        <div className="flex flex-col gap-8">
+
+          {/* first shopping item */}
         <div className="flex gap-4">
           <Image
             src="https://images.pexels.com/photos/773252/pexels-photo-773252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -38,6 +41,40 @@ const CartModal = () => {
               <span className="text-blue-500">Remove</span>
             </div>
           </div>
+        </div>
+
+          {/* second shopping item */}
+          <div className="flex gap-4">
+          <Image
+            src="https://images.pexels.com/photos/4194714/pexels-photo-4194714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={100}
+            height={80}
+            className="object-cover rounded-md"
+          />
+          <div className="flex flex-col justify-between w-full">
+            {/* top */}
+
+            <div className="">
+              {/* title */}
+
+              <div className="flex items-center justify-between gap-8">
+                <h3 className="font-semibold">Product Name</h3>
+                <div className="p-1 bg-gray-50 rounded-sm text-xs">$89</div>
+              </div>
+
+              {/* desc*/} 
+              <div className="text-sm text-gray-500 ">
+                Available
+              </div>
+            </div>
+            {/* bottom */}
+            <div className="flex justify-between text-sm">
+              <span className="text-gray-500">Qty 3</span>
+              <span className="text-blue-500">Remove</span>
+            </div>
+          </div>
+        </div>
         </div>
       )}
     </div>
