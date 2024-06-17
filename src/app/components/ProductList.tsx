36459d327d2c38aa03490fdca312c22cb6c34c14
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-17 13:36:35 +0530

    adjust the screen size

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const ProductList = () => {
   return (
     <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="w-full flex flex-col gap-4 ">
+      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]">
         <div className="relative w-full h-80">
           <Image
             src="https://images.pexels.com/photos/773252/pexels-photo-773252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
