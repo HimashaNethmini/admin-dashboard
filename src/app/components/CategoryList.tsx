ff815dfc4cf9dac45ad11fd1e897b7a4d8e3de29
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-17 15:59:19 +0530

    completed categoryList

--- src/app/components/CategoryList.tsx
+++ src/app/components/CategoryList.tsx
@@ -1,7 +1,104 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const CategoryList = () => {
   return (
-    <div>Categories</div>
-  )
-}
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-4 md:gap-8 ml-20">
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/3705539/pexels-photo-3705539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              fill
+              sizes="20vw"
+              className="rounded md"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
 
-export default CategoryList
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/3705539/pexels-photo-3705539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              fill
+              sizes="20vw"
+              className="rounded md"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/3705539/pexels-photo-3705539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              fill
+              sizes="20vw"
+              className="rounded md"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/3705539/pexels-photo-3705539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              fill
+              sizes="20vw"
+              className="rounded md"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.pexels.com/photos/3705539/pexels-photo-3705539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              fill
+              sizes="20vw"
+              className="rounded md"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-cl tracking-wide">
+            Category Name
+          </h1>
+        </Link>
+        
+      </div>
+      Categories
+    </div>
+  );
+};
+
+export default CategoryList;
