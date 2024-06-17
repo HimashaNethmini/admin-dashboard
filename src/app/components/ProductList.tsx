f3c24ce57cc6996c2ea6b5319818eaefd15aeb5d
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-17 13:34:45 +0530

    make it responsive

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -4,13 +4,23 @@ import Image from "next/image";
 const ProductList = () => {
   return (
     <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="relative w-full h-80">
-        <Image
-          src="https://images.pexels.com/photos/773252/pexels-photo-773252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          width={200}
-          height={200}
-        />
+      <Link href="/test" className="w-full flex flex-col gap-4 ">
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.pexels.com/photos/773252/pexels-photo-773252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+            <Image
+            src="https://images.pexels.com/photos/4194714/pexels-photo-4194714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
       </Link>
     </div>
   );
