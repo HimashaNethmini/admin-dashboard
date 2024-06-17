21d5c4bc6e51d58ae242524ef9cebf258b93adf4
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-17 13:48:49 +0530

    style button

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 const ProductList = () => {
   return (
     <div className="flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:[22%]">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[35%] lg:[22%]"
+      >
         <div className="relative w-full h-80">
           <Image
             src="https://images.pexels.com/photos/773252/pexels-photo-773252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -13,7 +16,7 @@ const ProductList = () => {
             sizes="25vw"
             className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
           />
-            <Image
+          <Image
             src="https://images.pexels.com/photos/4194714/pexels-photo-4194714.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
             fill
@@ -21,6 +24,14 @@ const ProductList = () => {
             className="absolute object-cover rounded-md"
           />
         </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product Name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl ring-1 ring-cart text-cart w-max py-2 px-4 text-xs hover:bg-cart hover:text-white">
+            Add to Cart
+        </button>
       </Link>
     </div>
   );
