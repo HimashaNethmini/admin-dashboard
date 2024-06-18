905ccbdc28ba9ca7e4cdeae2b0a49badcc83cd7c
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-18 14:59:07 +0530

    making the filter form

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -1,14 +1,30 @@
 const Filter = () => {
   return (
     <div className="mt-12 flex justify-between">
-        <div className="flex gap-6 flex-wrap">
-            <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]" onChange={handleFilterChange}>
-                
-
-            </select>
-        </div>
+      <div className="flex gap-6 flex-wrap">
+        <select
+          name="type"
+          id=""
+          className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+          onChange={handleFilterChange}
+        >
+            <option>Type</option>
+            <option value="physical">Physical</option>
+            <option value="digital">Digital</option>
+        </select>
+        <input type="text"
+            name="min"
+            placeholder="min price"
+            className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+            onChange={handleFilterChange} />
+        <input type="text"
+            name="max"
+            placeholder="max price"
+            className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
+            onChange={handleFilterChange} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
