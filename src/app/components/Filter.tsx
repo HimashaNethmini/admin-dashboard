4bb20bbf5a3c73f274c46c22ff34ad8747dd9640
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-18 14:53:04 +0530

    developing filter part

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -1,6 +1,13 @@
 const Filter = () => {
   return (
-    <div>Filter</div>
+    <div className="mt-12 flex justify-between">
+        <div className="flex gap-6 flex-wrap">
+            <select name="type" id="" className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]" onChange={handleFilterChange}>
+                
+
+            </select>
+        </div>
+    </div>
   )
 }
 
