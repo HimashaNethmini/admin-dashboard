176c556ef39ad52a132ec365238fc47a2c5c3edd
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-18 15:08:07 +0530

    developed the sorting

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -22,6 +22,36 @@ const Filter = () => {
             placeholder="max price"
             className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-400"
             onChange={handleFilterChange} />
+
+            {/* todo: filter categories */}
+            <select 
+                name="cat" 
+                className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
+                onChange={handleFilterChange}
+            >
+                <option>Category</option>
+                <option value="">New Arrival</option>
+                <option value="">Popular</option>
+            </select>
+            <select 
+                name="" 
+                id=""
+                className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]">
+                    <option>All Filters</option>
+            </select>
+      </div>
+      <div className="">
+        <select 
+            name="sort" 
+            id=""
+            className="py-2 px-4 rounded-2xl text-xs font-medium bg-white ring-1 ring-gray-400"
+            onChange={handleFilterChange}>
+                <option> Sort By</option>
+                <option value="asc price">Price (low to high)</option>
+                <option value="desc price">Price (high to low)</option>
+                <option value="asc lastUpdated">Newest</option>
+                <option value="desc lastUpdated">Oldest</option>
+            </select>
       </div>
     </div>
   );
