c722da99ef6ca5d2da35df32ad2899879529ec47
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-19 16:13:49 +0530

    completed images list

--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -41,7 +41,7 @@ const ProductImages = () => {
         {/* check images when clicking on it */}
         {images.map((img, i) => (
           <div 
-            className="w-1/4 h-32 relative gap-4 mt-8"
+            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
             key={img.id}
             onClick={()=> setIndex(i)} > 
               <Image 
