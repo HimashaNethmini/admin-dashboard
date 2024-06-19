3f5b4b13b8dc5a3f60e50a3870afb97570931664
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-19 18:03:03 +0530

    make the page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,7 +1,44 @@
+import Add from "../components/Add";
+import CustomizedProducts from "../components/CustomizedProducts";
+import ProductImages from "../components/ProductImages";
+
 const SinglePage = () => {
-    return (
-      <div> Single Page</div>
-    )
-  }
-  
-  export default SinglePage
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
+      {/* img */}
+      <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
+        <ProductImages />
+      </div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          crazy adjective · intensely enthusiastic about or preoccupied with.
+          Synonyms. dotty, dotty, gaga, wild · possessed by inordinate
+          excitement
+        </p>
+        <div className="h-[2px] bg-gray-100">
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">$59</h3>
+            <h2 className="font-medium text-2xl">$49</h2>
+          </div>
+          <div className="h-[2px] bg-gray-100">
+            <CustomizedProducts />
+            <Add />
+            <div className="h-[2px] bg-gray-100">
+              <div className="text-sm">
+                <h4 className="font-medium mb-4">Title</h4>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s ...
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePage;
