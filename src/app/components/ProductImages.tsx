6f72c624d9560c1cc9d5b88893390095a4973183
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-19 16:09:32 +0530

    check images when clicking on it

--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 //create a list of images
 const images = [
@@ -7,60 +10,49 @@ const images = [
     url: "https://images.pexels.com/photos/7032143/pexels-photo-7032143.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    id:2,
-    url:"https://images.pexels.com/photos/5938598/pexels-photo-5938598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    id: 2,
+    url: "https://images.pexels.com/photos/5938598/pexels-photo-5938598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    id:3,
-    url:"https://images.pexels.com/photos/208483/pexels-photo-208483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    id: 3,
+    url: "https://images.pexels.com/photos/208483/pexels-photo-208483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    id:4,
-    url:"https://images.pexels.com/photos/4202924/pexels-photo-4202924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  }
+    id: 4,
+    url: "https://images.pexels.com/photos/4202924/pexels-photo-4202924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
 ];
 
 const ProductImages = () => {
+  //use effect to change
+  const [index, setIndex] = useState(0);
   return (
     <div className="">
       <div className="h-[500px] relative">
         <Image
-          src="https://images.pexels.com/photos/1460839/pexels-photo-1460839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={images[index].url}
           alt=""
           fill
-          sizes="50vw"
+          sizes="70vw"
           className="object-cover rounded-md"
         />
       </div>
-      <div className="">
-        <div className="w-1/2 h-32 relative gap-4 mt-8">
-          <Image
-            src="https://images.pexels.com/photos/1460839/pexels-photo-1460839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="30vw"
-            className="object-cover rounded-md"
-          />
-        </div>
-        <div className="">
-          <Image
-            src="https://images.pexels.com/photos/1460839/pexels-photo-1460839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="30vw"
-            className="object-cover rounded-md"
-          />
-        </div>
-
-        <div className="">
-          <Image
-            src="https://images.pexels.com/photos/1460839/pexels-photo-1460839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            fill
-            sizes="30vw"
-            className="object-cover rounded-md"
-          />
-        </div>
+      <div className="flex justify-between gap-4 mt-8">
+        {/* check images when clicking on it */}
+        {images.map((img, i) => (
+          <div 
+            className="w-1/4 h-32 relative gap-4 mt-8"
+            key={img.id}
+            onClick={()=> setIndex(i)} > 
+              <Image 
+                src={img.url}
+                alt=""
+                fill
+                sizes="30vw"
+                className="object-cover rounded-md"
+              />
+            </div>
+        ))}
       </div>
     </div>
   );
