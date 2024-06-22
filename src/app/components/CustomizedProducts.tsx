2ea7fb9b57e68dac5a88cbc864f1ae0784f423c3
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-22 22:25:53 +0530

    add a customized page

---
+++ src/app/components/CustomizedProducts.tsx
@@ -0,0 +1,12 @@
+const CustomizedProducts = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <h4> Choose a colour</h4>
+      <div className=""></div>
+      <h4>Choose a size</h4>
+      <div className=""></div>
+    </div>
+  )
+}
+
+export default CustomizedProducts
