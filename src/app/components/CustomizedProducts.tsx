46f9123c384a4f5180ec541793eb39ed27c9b479
HimashaNethmini <himashanethmini05@gmail.com>
2024-06-22 22:35:33 +0530

    added styles

--- src/app/components/CustomizedProducts.tsx
+++ src/app/components/CustomizedProducts.tsx
@@ -1,10 +1,12 @@
 const CustomizedProducts = () => {
   return (
     <div className="flex flex-col gap-6">
-      <h4> Choose a colour</h4>
-      <div className=""></div>
-      <h4>Choose a size</h4>
-      <div className=""></div>
+      <h4 className="font-medium"> Choose a colour</h4>
+      <div className="flex items-center gap-3">
+        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500"></li>
+      </div>
+      <h4 className="font-medium">Choose a size</h4>
+      <div className="flex items-center gap-3"></div>
     </div>
   )
 }
