c7172637ae3a89e3b60cb20e9f92f5a69758bd61
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-05 15:18:40 +0530

    debugged

--- src/app/components/Navicons.tsx
+++ src/app/components/Navicons.tsx
@@ -54,15 +54,17 @@ const Navicons = () => {
       />
 
     {/*cart handling */}
-    <div className="relative cursor-pointer">
+    <div 
+      className="relative cursor-pointer"
+      onClick={() => setIsCartOpen((prev) => !prev)}>
+
       <Image
         src="/cart.png"
         alt=""
         width={22}
-        height={22}
-        className="cursor-pointer"
-        onClick={() => setIsCartOpen((prev) => !prev)}
+        height={22} 
       />
+      
       <div className="absolute -top-4 -right-3 w-5 h-6 m-1  bg-cart text-xs text-white rounded-full flex items-center justify-center">2</div>
       </div>
       {isCartOpen && <CartModal/>}
