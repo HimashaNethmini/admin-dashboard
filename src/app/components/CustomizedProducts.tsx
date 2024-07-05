49e4d3d05d59e3163780a0ad28311974db2d7581
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-05 11:59:51 +0530

    styles added

--- src/app/components/CustomizedProducts.tsx
+++ src/app/components/CustomizedProducts.tsx
@@ -2,22 +2,23 @@ const CustomizedProducts = () => {
   return (
     <div className="flex flex-col gap-6">
       <h4 className="font-medium"> Choose a colour</h4>
-      <div className="flex items-center gap-3">
+      <ul className="flex items-center gap-3">
         <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-500">
           {/* the circle in colour  */}
-          <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform-translate-x-1/2 -translate-y-1/2 "/>
-        </li>
+          <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform-translate-x-1/2 -translate-y-1/2 "/></li>
 
-        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500">
-        </li>
-
+        <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-blue-500"></li>
         <li className="w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-green-500">
           <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform-translate-x-1/2 -translate-y-1/2 "/>
         </li>
-
-      </div>
+      </ul>
       <h4 className="font-medium">Choose a size</h4>
-      <div className="flex items-center gap-3"></div>
+      <ul className="flex items-center gap-3 md:font-bold">
+        <li className="ring-1 ring-cart text-cart rounded-md py-1 px-4 text-sm cursor-pointer"> Small</li>
+        <li className="ring-1 ring-blu text-blu rounded-md py-1 px-4 text-sm cursor-pointer"> Medium</li>
+        <li className="ring-1 ring-gren text-gren rounded-md py-1 px-4 text-sm cursor-pointer"> Large</li>
+
+      </ul>
     </div>
   );
 };
