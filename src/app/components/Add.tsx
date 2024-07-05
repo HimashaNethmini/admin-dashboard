0fff39ea6b425caba420204f350ca9f7b8f7b1f6
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-05 14:07:11 +0530

    function to decrease Quantity

--- src/app/components/Add.tsx
+++ src/app/components/Add.tsx
@@ -5,22 +5,26 @@ import { useState } from "react";
 const Add = () => {
   //add the quantity part
   const [Quantity, setQuantity] = useState(1);
-  
+
   //increment function
   const incrementQuantity = () => {
-    setQuantity(prev => prev + 1);
-  }
+    setQuantity((prev) => prev + 1);
+  };
+
+  //decrease Quantity
+  const decrementQuantity = () => {
+    setQuantity((prev) => (prev > 1 ? prev - 1 : 1));
+  };
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 mt-3">
       <h4 className="font-medium">Choose a Quantity</h4>
       <div className="flex justify-between">
         <div className="flex items-center gap-4">
           <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
             <button
               className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
-              onClick = { }
-              // disabled={quantity===1}
+              onClick={decrementQuantity}
             >
               -
             </button>
@@ -28,25 +32,21 @@ const Add = () => {
             <button
               className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
               onClick={incrementQuantity}
-              // disabled={quantity===stockNumber}
             >
               +
             </button>
           </div>
-          {/* {stockNumber < 1 ? ( */}
-            <div className="text-xs">{/*Product is out of stock*/}</div>
-          {/* // ) : ( */}
+          {Quantity === 1 && (
             <div className="text-xs">
-              Only <span className="text-orange-500">{/*{stockNumber}*/} items</span>{" "}
-              left!
+              Only <span className="text-orange-500"> items</span> left!
               <br /> {"Don't"} miss it
             </div>
-          {/* )} */}
+          )}
         </div>
         <button
           // onClick={() => addItem(wixClient, productId, variantId, quantity)}
           // disabled={isLoading}
-          className="w-36 text-sm rounded-3xl ring-1 ring-lama text-lama py-2 px-4 hover:bg-lama hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
+          className="w-36 text-sm rounded-3xl ring-1 ring-cart text-cart py-2 px-4 hover:bg-cart hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none"
         >
           Add to Cart
         </button>
