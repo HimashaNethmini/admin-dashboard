028a8e75386729610f2f9ccbae9b882d0d476cb0
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-05 13:03:13 +0530

    add to cart component

--- src/app/components/Add.tsx
+++ src/app/components/Add.tsx
@@ -1,6 +1,12 @@
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className="flex flex-col gap-4">
+      <h4 className="font-medium mt-3"> Choose a Quantity</h4>
+      <div className=""></div>
+      <button className="w-36 text-sm rounded-3xl ring-1 ring-cart text-cart py-2 hover:bg-cart hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        Add to Cart
+      </button>
+    </div>
   )
 }
 
