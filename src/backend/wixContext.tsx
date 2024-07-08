3b86e1aeaec2d3e0c41ef382f969b5daf56e55f4
HimashaNethmini <himashanethmini05@gmail.com>
2024-07-08 14:03:15 +0530

    create the cookie token

--- src/backend/wixContext.tsx
+++ src/backend/wixContext.tsx
@@ -2,23 +2,22 @@
 
 import { createClient, OAuthStrategy } from "@wix/sdk";
 import { products, collections } from "@wix/stores";
+import Cookies from "js-cookie";
+
+//checking refresh tokens
+//inside cookies is a string, transforming it into a json object
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}");
 
 const myWixClient = createClient({
-    modules: {
-      products,
+  modules: {
+    products,
     //   currentCart
+  },
+  auth: OAuthStrategy({
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+    tokens: {
+      refreshToken,
+      accessToken: { value: "", expiresAt: 0 },
     },
-    auth: OAuthStrategy({
-      clientId: '<YOUR_CLIENT_ID>',
-      tokens: {
-        accessToken: {
-          value: '<ACCESS_TOKEN_VALUE>',
-          expiresAt: <ACCESS_TOKEN_EXPIRY_DATE>
-
-        },
-        refreshToken: {
-          value: '<REFRESH_TOKEN_VALUE>'
-        }
-      }
-    })
-  })
+  }),
+});
